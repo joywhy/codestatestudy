a986dab0dbe82e73395b601874266c73754b1e08
joywhy <leh9111@naver.com>
2023-03-27 04:51:28 +0900

    리액트 이벤트처리 연습

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter,Route,Routes, link } from "react-router-dom";
 import './App.css';
+import Tweets from "./Tweets";
+
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <BrowserRouter>
+  <div>
+    <div className='App'>
+      <main>
+        
+        <Routes>
+          <Route path="/" element={<Tweets/>}/>
+
+
+        </Routes>
+      </main>
+
+
     </div>
+  </div>
+  </BrowserRouter>
   );
 }
 
